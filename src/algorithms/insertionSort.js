49afee79317ea161d982af77ddda68dd0624c462
Spesghet <liudaniel0322@gmail.com>
2024-08-23 16:01:57 -0400

    added visual components

--- src/algorithms/insertionSort.js
+++ src/algorithms/insertionSort.js
@@ -1,6 +1,20 @@
 function insertionSort(arr, visualize){
     for(i = 1; i < arr.length; i++){
-        
-
+        let j = i - 1; 
+        let value = arr[i]
+        while(j >= 0 && arr[j] > value){
+            arr[j+1] = arr[j];
+            j = j - 1;
+            if (visualize) {
+                visualize([...arr]);
+            }
+        }
+    arr[j + 1] = value
+    if (visualize) {
+        visualize([...arr]);
     }
+    }
+    return arr;
+
 }
+export default insertionSort
